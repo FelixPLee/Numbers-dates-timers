0c413259a0f32a6da21c0bade143d3763ff171bf
FelixPLee <felixpinheirolee@gmail.com>
2025-03-14 23:27:12 -0300

    refactoring display movements and adding curr dates

--- script.js
+++ script.js
@@ -81,10 +81,10 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
@@ -142,7 +142,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -153,7 +153,18 @@ const updateUI = function (acc) {
 
 ///////////////////////////////////////
 // Event handlers
-let currentAccount;
+let currentAccount = account1;
+updateUI(currentAccount)
+containerApp.style.opacity = 100
+
+const now = new Date()
+const day = `${now.getDate()}`.padStart(2,0)
+const month = `${now.getMonth() +1}`.padStart(2,0)
+const year = now.getFullYear()
+const hour = now.getHours()
+const min = now.getMinutes()
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`
+
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -244,7 +255,7 @@ btnClose.addEventListener('click', function (e) {
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
 
