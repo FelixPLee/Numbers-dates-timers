56ba320fefcd0250a7abd71aa473a7dfd3a5424d
FelixPLee <felixpinheirolee@gmail.com>
2025-04-01 23:23:32 -0300

    localizing values

--- script.js
+++ script.js
@@ -97,6 +97,15 @@ const formatMovementDate = function(date, locale){
       return new Intl.DateTimeFormat(locale).format(date)
 }
 
+const fomratCur = function(value, locale, currency) {
+
+  const formattedMov = new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency
+  }).format(value)
+  return formattedMov
+}
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
   
@@ -111,13 +120,16 @@ const displayMovements = function (acc, sort = false) {
       const date = new Date(movementDate)
       const displayDate  =formatMovementDate(date, acc.locale)
 
+      const formattedMov = fomratCur(obj.movement, acc.locale, acc.currency)
+      console.log(formattedMov)
+
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
         <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${movement.toFixed(2)}€</div>
+        <div class="movements__value">${formattedMov}</div>
       </div>
     `;
 
@@ -127,19 +139,19 @@ const displayMovements = function (acc, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
+  labelBalance.textContent = fomratCur(acc.balance, acc.locale, acc.currency);
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+  labelSumIn.textContent = fomratCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
+  labelSumOut.textContent = fomratCur(Math.abs(out), acc.locale, acc.currency);
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -149,7 +161,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+  labelSumInterest.textContent = fomratCur(interest, acc.locale, acc.currency);
 };
 
 const createUsernames = function (accs) {
